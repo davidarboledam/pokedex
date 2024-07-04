c0c4d1c63047038d13661d66d5bafdcbb4b1f17b
David Arboleda-Mendez <davida.arboledam@gmail.com>
2024-07-03 23:35:09 -0500

    Se actualiza siguiendo los requerimientos del docente

--- Components/shared/PokeHeader.jsx
+++ Components/shared/PokeHeader.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./styles/pokeheader.css"
+import pokeHeadImg from "../../src/assets/Images/pokedexhead.png"
 
 export const PokeHeader = () => {
   return (
     <div className='pokeheader'>
         <div className='pokeheader__red'>
             <figure className='pokeheader__img'>
-                <img src="../../src/assets/Images/pokedexhead.png" />
+                <img src={pokeHeadImg} />
             </figure>
         </div>
         <div className='pokeheader__black'>
